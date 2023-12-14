3230df4727ed17c65c5676bfc6f659b41d4fa73d
Evert <evertdevroey@gmail.com>
2023-12-14 12:08:42 +0100

    update system dynamics

--- src/toy-problem.py
+++ src/toy-problem.py
@@ -7,17 +7,38 @@ import matplotlib.pyplot as plt
 
 class System:
     def __init__(self, m, k, c, l0=0, g=9.81):
+        self.m = m
+        self.k = k
+        self.c = c
+        self.l0 = l0
+        self.g = g
+        self.u = m * g + k * l0
+
         self.A = np.array([[0., 1.], [-k / m, -c / m]])
         self.B = np.array([[0., 1 / m]]).T
         self.C = np.array([[1., 0.]])
         self.D = np.array([[0.]])
         self.sys = sp.signal.StateSpace(self.A, self.B, self.C, self.D)
-        self.u = m * g + k * l0
 
-    def response(self, s, a=0, dt=0.01):
+    def response(self, s, a=0, dt=0.01, do_update=False):
         _, _, s = sp.signal.lsim(self.sys, 2 * [self.u + a], [0, dt], s)
 
+        if do_update:
+            self.update()
+
         return s[-1]
+
+    def update(self):
+        self.k *= 0.9
+        self.c *= 0.9
+        self.l0 += 1 / self.l0
+        self.u = self.m * self.g + self.k * self.l0
+
+        self.A = np.array([[0., 1.], [-self.k / self.m, -self.c / self.m]])
+        self.B = np.array([[0., 1 / self.m]]).T
+        self.C = np.array([[1., 0.]])
+        self.D = np.array([[0.]])
+        self.sys = sp.signal.StateSpace(self.A, self.B, self.C, self.D)
 
 
 class PIDController:
@@ -40,20 +61,20 @@ class PIDController:
 np.random.seed(15)
 
 system = System(5, 10, 3, 5)
-controller = PIDController(0.35*600, 1.5*600*0.57/8, 2*0.6*600/0.57)  # w/ kp_ult = 600 and T_ult = 0.57
+controller = PIDController(0.35 * 600, 1.5 * 600 * 0.57 / 8, 2 * 0.6 * 600 / 0.57)  # w/ kp_ult = 600 and T_ult = 0.57
 
 dt = 0.01
 x0 = [9.9, 0]  # 9.9 was found to be the steady state
 signal = []
 targets = []
-t = np.arange(0, 50, dt)
+t = np.arange(0, 60, dt)
 
 for ti in t:
     if ti % 15 == 0:
         target = np.random.rand(1) * 6 + 7
     targets.append(target)
     a = controller.compute_control(x0, target, dt)
-    x = system.response(x0, a)
+    x = system.response(x0, a, do_update=True)
     signal.append(x)
     x0 = x
 
