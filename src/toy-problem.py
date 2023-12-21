09285819f9e62aba4e96f0be70c2c43528c55ca8
Evert-DV <evertdevroey@gmail.com>
2023-12-21 10:07:34 +0100

    error prediction and adaptation approach

--- src/toy-problem.py
+++ src/toy-problem.py
@@ -2,10 +2,11 @@ import os
 import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
 
 
 # Create a mass-spring-damper toy problem
-
 class System:
     def __init__(self, m, k, c, l0=0, g=9.81):
         self.m = m
@@ -59,11 +60,25 @@ class PIDController:
         return control_action
 
 
+class Adapter(nn.Sequential):
+    def __init__(self, input_size, output_size):
+        super(Adapter, self).__init__(
+            nn.Linear(input_size, 32),
+            nn.Softsign(),
+            nn.Linear(32, output_size),
+            nn.ReLU()
+        )
+
+
 def main():
     np.random.seed(15)
 
     system = System(5, 10, 3, 5)
     controller = PIDController(350, 107.5, 1257)
+    adapter = Adapter(3, 1)
+
+    optimizer = torch.optim.SGD(adapter.parameters(), lr=0.001)
+    loss_fn = nn.MSELoss()
 
     dt = 1 / 60
     x0 = [9.9, 0]  # 9.9 was found to be the steady state
@@ -73,17 +88,42 @@ def main():
     t = np.arange(0, 45, dt)
     target = None
     buffer = []
+    x0_adj = x0
 
     for ti in t:
-        if ti % 15 == 0:
+        if ti % 10 == 0 and ti != 0:
+            print("Fitting model...")
+            buffer = np.asarray(buffer)
+            features = buffer[:, :3]
+            e = buffer[:, -3:-2] - buffer[:, -1:]
+            features = torch.from_numpy(features).float()
+            e = torch.from_numpy(e).float()
+
+            for epoch in range(100):
+                print(f"\rEpoch {epoch}", end="")
+                optimizer.zero_grad()
+                output = adapter(features)
+                loss = loss_fn(output, e)
+                loss.backward()
+                optimizer.step()
+
+            buffer = []
+
+        if ti % 45 == 0:
             target = np.random.rand(1) * 6 + 7
         targets.append(target)
-        a = controller.compute_control(x0, target, dt)
+        a = controller.compute_control(x0_adj, target, dt)
         controls.append(a)
         x = system.response(x0, a, do_update=True)
+        e = adapter(torch.tensor([*x0, *a]).float())
+        x_adj = x
+        x_adj[0] += e.item()
+
         signal.append(x)
         buffer.append([*x0, *a, *x, *target])
+
         x0 = x
+        x0_adj = x_adj
 
     signal = np.asarray(signal)
 
@@ -102,5 +142,9 @@ def main():
     fig.savefig("./tmp/plot.png", dpi=300)
 
 
-if __name__ is "__main__":
-    main()
+if __name__ == "__main__":
+    if torch.cuda.is_available():
+        with torch.cuda.device(0):
+            main()
+    else:
+        main()
