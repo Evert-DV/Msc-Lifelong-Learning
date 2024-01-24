59fd97b53ffdeea46cf3d313724c246b23fd98bf
Evert-DV <evertdevroey@gmail.com>
2024-01-24 16:59:19 +0100

    Migration to keras
    migrated to keras, using torch backend

--- src/toy-problem.py
+++ src/toy-problem.py
@@ -1,13 +1,17 @@
 import os
+
+os.environ["KERAS_BACKEND"] = "torch"
 import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
 import torch
-import torch.nn as nn
+import keras
+from keras import layers
+from keras.models import load_model, save_model
 from torch.utils.data import DataLoader, TensorDataset
+from copy import deepcopy
 
 
-# Create a mass-spring-damper toy problem
 class System:
     def __init__(self, m, k, c, l0=0, g=9.81):
         self.m = m
@@ -61,72 +65,95 @@ class PIDController:
         return control_action
 
 
-class Adapter(nn.Sequential):
+class Adapter(keras.Sequential):
     def __init__(self, input_size, output_size):
-        super(Adapter, self).__init__(
-            nn.Linear(input_size, 32),
-            nn.Softsign(),
-            nn.Linear(32, 32),
-            nn.LeakyReLU(),
-            nn.Linear(32, output_size),
-        )
+        super(Adapter, self).__init__()
+        self.add(layers.Dense(32, activation='softmax'))
+        self.add(layers.Dense(32, activation='leaky_relu'))
+        self.add(layers.Dense(output_size))
 
 
 def main():
-    pretrain = False
+    pretrain = True
     seed = np.random.randint(0, 1000)
     print(f"Seed: {seed}")
     np.random.seed(seed)
     if pretrain:
         np.random.seed(16)
 
-    torch.manual_seed(16)
-
     system = System(5, 10, 3, 5)
     controller = PIDController(350, 107.5, 1257)
-    default_controller = PIDController(350, 107.5, 1257)
+    reference_controller = PIDController(350, 107.5, 1257)
 
     prediction_window = 10
-    adapter = Adapter(4, prediction_window)
+    # adapter = Adapter(4, prediction_window)
+    adapter = Adapter(4, 2)
     if not pretrain:
-        adapter.load_state_dict(torch.load('./tmp/adapter_state_dict.pth'))
+        load_model('./tmp/target_adapter.keras')
         # pass
-    optimizer = torch.optim.Adam(adapter.parameters(), lr=1e-2)
-    loss_fn = nn.MSELoss()
+    optimizer = keras.optimizers.Adam(learning_rate=1.e-2)
+    loss_fn = keras.losses.MeanSquaredError()
+    adapter.compile(optimizer=optimizer, loss=loss_fn)
 
     if pretrain:
         pretrain_data = np.load("./tmp/pretrain_data.npy")
 
         print("\nPretraining model...")
-        adapter.train()
-        features = np.concatenate(
-            (pretrain_data[:-prediction_window, [0, 1]], pretrain_data[prediction_window:, [3, 4]]),
-            axis=1)  # state transitions
-        label_action = np.asarray(
-            [pretrain_data[i:i + prediction_window, 2:3].ravel() for i in
-             range(len(pretrain_data) - prediction_window)])  # control actions as labels
-        dataset = TensorDataset(torch.from_numpy(features).float(), torch.from_numpy(label_action).float())
+        # features = np.concatenate(
+        #     (pretrain_data[:-prediction_window, [0, 1]], pretrain_data[prediction_window:, [3, 4]]),
+        #     axis=1)  # state transitions
+        # labels = np.asarray(
+        #     [pretrain_data[i:i + prediction_window, 2:3].ravel() for i in
+        #      range(len(pretrain_data) - prediction_window)])  # control actions as labels
+        features = pretrain_data[:, [0, 1, 3, 4]]
+        labels = pretrain_data[:, -2:]
+        dataset = TensorDataset(torch.from_numpy(features), torch.from_numpy(labels))
         dataloader = DataLoader(dataset, batch_size=256, shuffle=True)
 
-        for epoch in range(100):
-            for inputs, targets in dataloader:
-                optimizer.zero_grad()
-                output = adapter(inputs)
-                loss = loss_fn(output, targets)
-                loss.backward()
-                optimizer.step()
-            print(f"\rEpoch {epoch}\t Loss: {loss.item():.2f}", end="")
+        callback = keras.callbacks.EarlyStopping(monitor='loss',
+                                                 mode='min',
+                                                 min_delta=1e-4,
+                                                 patience=5)
 
-        torch.save(adapter.state_dict(), './tmp/adapter_state_dict.pth')
+        history = adapter.fit(dataloader,
+                              epochs=100,
+                              batch_size=32,
+                              callbacks=[callback])
+
+        # best_loss = float('inf')
+        # best_params = None
+        # old_loss = float('inf')
+        #
+        # for epoch in range(100):
+        #     for inputs, targets in dataloader:
+        #         optimizer.zero_grad()
+        #         output = adapter(inputs)
+        #         loss = loss_fn(output, targets)
+        #         loss.backward()
+        #         optimizer.step()
+        #
+        #     print(f"\rEpoch {epoch}\t Loss: {loss.item():.4f}", end="")
+        #
+        #     if loss.item() < best_loss:
+        #         best_loss = loss.item()
+        #         best_params = deepcopy(adapter.state_dict())
+        #
+        #     if abs(old_loss - loss.item()) < 1e-4:
+        #         break
+        #
+        #     old_loss = loss.item()
+
+        save_model(adapter, './tmp/target_adapter.keras')
 
     dt = 1 / 60
     x0 = [9.9, 0]  # 9.9 was found to be the steady state
     signal = []
     signal_naive = []
     targets = []
-    default_controls = []
+    reference_controls = []
     adapted_controls = []
-    predicted_controls = []
+    adapted_targets = []
+    predicted_targets = []
     t = np.arange(0, 240, dt)
     target = np.array([11., 0.])
     buffer = []
@@ -134,50 +161,63 @@ def main():
 
     if not pretrain:
         for ti in t:
-            if ti % 15 == 0 and ti != 0:
-                print("\nFitting model...")
-                adapter.train()
-                buffer = np.asarray(buffer)
-                features = np.concatenate((buffer[:-prediction_window, [0, 1]], buffer[prediction_window:, [3, 4]]),
-                                          axis=1)  # state transitions
-                label_action = np.asarray(
-                    [buffer[i:i + prediction_window, 2:3].ravel() for i in
-                     range(len(buffer) - prediction_window)])  # control actions as labels
-                dataset = TensorDataset(torch.from_numpy(features).float(), torch.from_numpy(label_action).float())
-                X, y = dataset.tensors
+            # if ti % 15 == 0 and ti != 0:
+            #     print("\nFitting model...")
+            #     adapter.train()
+            #     buffer = np.asarray(buffer)
+            #     # features = np.concatenate((buffer[:-prediction_window, [0, 1]], buffer[prediction_window:, [3, 4]]),
+            #     #                           axis=1)  # state transitions
+            #     # labels = np.asarray(
+            #     #     [buffer[i:i + prediction_window, 2:3].ravel() for i in
+            #     #      range(len(buffer) - prediction_window)])  # control actions as labels
+            #     features = buffer[:, [0, 1, 3, 4]]
+            #     labels = buffer[:, -2:]
+            #     dataset = TensorDataset(torch.from_numpy(features).float(), torch.from_numpy(labels).float())
+            #     X, y = dataset.tensors
+            #
+            #     for epoch in range(100):
+            #         optimizer.zero_grad()
+            #         output = adapter(X)
+            #         loss = loss_fn(output, y)
+            #         loss.backward()
+            #         optimizer.step()
+            #         print(f"\rEpoch {epoch}\t Loss: {loss:.2f}", end="")
+            #
+            #     buffer = []
 
-                for epoch in range(100):
-                    optimizer.zero_grad()
-                    output = adapter(X)
-                    loss = loss_fn(output, y)
-                    loss.backward()
-                    optimizer.step()
-                    print(f"\rEpoch {epoch}\t Loss: {loss:.2f}", end="")
-
-                buffer = []
-
-            if ti % 20 == 0:
+            if ti % 15 == 0:
                 target = [np.random.rand() * 6 + 7, 0.]
 
             adapter.eval()
-            targets.append(target)
-            control_action = controller.compute_control(x0, target, dt)
-
-            if ti > 15:
-                predicted_action = adapter(torch.tensor([*x0, *target]).float())
-                adjustment = predicted_action[0].item() - control_action
-                predicted_controls.append(predicted_action[0].item())
+            if ti > 15.:
+                adapted_target = adapter(torch.tensor([control_action, *target]).float())
+                adapted_target = adapted_target.detach().numpy()
+                adapted_targets.append(adapted_target)
             else:
-                adjustment = 0
-                predicted_controls.append(np.nan)
+                adapted_target = target
+                adapted_targets.append(2 * [float('nan')])
 
-            control_action += adjustment
+            targets.append(target)
+            control_action = controller.compute_control(x0, adapted_target, dt)
 
-            a_naive = default_controller.compute_control(x0_naive, target, dt)
-            default_controls.append(a_naive)
+            # if ti > 15:
+            #     predicted_action = adapter(torch.tensor([*x0, *target]).float())
+            #     adjustment = predicted_action[0].item() - control_action
+            #     predicted_controls.append(predicted_action[0].item())
+            # else:
+            #     adjustment = 0
+            #     predicted_controls.append(np.nan)
 
-            x = system.response(x0, control_action, do_update=True)
+            # control_action += adjustment
+
+            a_naive = reference_controller.compute_control(x0_naive, target, dt)
+            reference_controls.append(a_naive)
+
+            x = system.response(x0, control_action, do_update=False)
             x_naive = system.response(x0_naive, a_naive, do_update=False)
+
+            prediction = adapter(torch.tensor([control_action, *x]).float())
+            predicted_targets.append(prediction.detach().numpy())
 
             adapted_controls.append(control_action)
 
@@ -191,9 +231,10 @@ def main():
         signal = np.asarray(signal)
         signal_naive = np.asarray(signal_naive)
         targets = np.asarray(targets)
-        labels = default_controls
+        predicted_targets = np.asarray(predicted_targets)
+        adapted_targets = np.asarray(adapted_targets)
 
-        fig, ax = plt.subplots(2, 1, sharex=True)
+        fig, ax = plt.subplots(3, 1, sharex=True)
 
         ax[0].plot(t, signal[:, 0], label="Adaptive controller")
         ax[0].plot(t, signal_naive[:, 0], label="Default controller")
@@ -202,12 +243,14 @@ def main():
         ax[0].legend()
 
         ax[1].plot(t, adapted_controls, label="Adapted control actions")
-        ax[1].plot(t, labels, '--', label="Default control actions")
+        ax[1].plot(t, reference_controls, '--', label="Default control actions")
         ax[1].legend()
 
-        # ax[2].plot(t, predicted_controls, label="Predicted control actions")
-        # ax[2].plot(t, adapted_controls, '--', label="Control actions (labels)")
-        # ax[2].legend()
+        ax[2].plot(t, targets[:, 0], label="Reference targets")
+        ax[2].plot(t, predicted_targets[:, 0], '--', label="Predicted targets")
+        ax[2].plot(t, adapted_targets[:, 0], label="Adapted targets")
+        ax[2].invert_yaxis()
+        ax[2].legend()
 
         fig.tight_layout()
         if not os.path.exists("./tmp"):
@@ -217,7 +260,9 @@ def main():
 
 
 if __name__ == "__main__":
+    print("Using backend " + keras.backend.backend())
     if torch.cuda.is_available():
+        print("Using CUDA")
         with torch.cuda.device(0):
             main()
     else:
