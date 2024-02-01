a3b19bec7e4933d238f932dbbdc63bcd0d59e1a2
Evert-DV <evertdevroey@gmail.com>
2024-02-01 17:02:57 +0100

    dynamics comparison
    function axtraction into train and compare
    represent dynamics as means and stds of abstract embeddings
    z score to define likelihood of emmbeddings belonging to same distribution

--- src/toy problem/toy_kb.py
+++ src/toy problem/toy_kb.py
@@ -8,16 +8,23 @@ import keras
 from keras import layers, optimizers, losses, ops
 
 
-def main():
-    seed = np.random.randint(0, 1000)
-    seed = 42
-    print(f"Seed: {seed}")
-    np.random.seed(seed)
-    torch.manual_seed(seed)
+def representation(x, encoder):
+    embeddings = encoder(x)
+    mean = ops.mean(embeddings, axis=0)
+    std = ops.std(embeddings, axis=0)
+    return mean, std
 
+
+def z_score(means, stds, sizes):
+    z = (means[0] - means[1])/ops.sqrt(stds[0]**2/sizes[0] + stds[1]**2/sizes[1])
+
+    return z
+
+
+def train():
     model_location = 'tmp/autoencoder.keras'
 
-    pretrain = True
+    pretrain = False
     if pretrain:
         encoder = keras.Sequential([
             layers.Input(shape=(5,)),
@@ -30,7 +37,6 @@ def main():
         decoder = keras.Sequential([
             layers.Input(shape=(3,)),
             layers.Dense(32, activation='relu'),
-            layers.Dropout(0.25),
             layers.Dense(5),
         ])
 
@@ -43,11 +49,11 @@ def main():
         encoder = autoencoder.layers[0]
         decoder = autoencoder.layers[-1]
 
-    optimizer = optimizers.Adam(learning_rate=1.e-5)
+    optimizer = optimizers.Adam(learning_rate=1.e-4)
     loss_fn = losses.MeanSquaredError()
     autoencoder.compile(optimizer=optimizer, loss=loss_fn)
 
-    data = np.load("tmp/train data/pretrain_data_m5.npy")
+    data = np.load(f"tmp/train data/pretrain_data_w_update_m5k10c3_seed934.npy")
     interval = 10
     windowed_data = ops.array(
         [data[i:i + interval * 60] for i in range(0, len(data) - interval * 60 + 1)[::interval * 60]])
@@ -56,7 +62,7 @@ def main():
     labels = windowed_data[..., [0, 1, 2, 3, 4]].reshape(-1, 5)
 
     dataset = TensorDataset(features, labels)
-    train_size = int(0.8 * len(dataset))
+    train_size = int(0.7 * len(dataset))
     val_size = len(dataset) - train_size
     train_set, val_set = random_split(dataset, [train_size, val_size])
     train_dataloader = DataLoader(train_set, batch_size=256, shuffle=True)
@@ -77,10 +83,42 @@ def main():
 
     autoencoder.save(model_location)
 
-    # reconstruction losses
-    # for i in range(len(labels))[::15*60]:
-    #     losses.append(loss_fn(labels[i:i+15*60], autoencoder.predict(features[i:i+15*60], verbose=0)).item())
-    # TODO: check linear independence test embeddings
+
+def compare():
+    model_location = 'tmp/autoencoder.keras'
+    autoencoder = keras.models.load_model(model_location)
+    encoder = autoencoder.layers[0]
+
+    means = []
+    stds = []
+    for file in os.listdir("tmp/train data"):
+        if file.endswith(".npy"):
+            data = np.load(f"tmp/train data/{file}")
+            interval = 10
+            windowed_data = ops.array(
+                [data[i:i + interval * 60] for i in range(0, len(data) - interval * 60 + 1)[::interval * 60]])
+
+            features = windowed_data[..., [0, 1, 2, 3, 4]].reshape(-1, 5)
+
+            mean, std = representation(features, encoder)
+            print(f"File: {file}\n"
+                  f"Mean: {mean.tolist()}")  #\tStd: {std.tolist()}\n")
+            # TODO: check statistical tests for comparison of distributions
+            means.append(mean)
+            stds.append(std)
+
+    print(f"Means: {means}")
+
+
+def main():
+    seed = np.random.randint(0, 1000)
+    seed = 42
+    print(f"Seed: {seed}")
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+
+    # train()
+    compare()
 
 
 if __name__ == '__main__':
