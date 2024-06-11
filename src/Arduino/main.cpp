c88fa25c9bc56f431124b069e238753689c09a88
Evert-DV <evertdevroey@gmail.com>
2024-06-11 16:08:18 +0200

    cleanup PID control.cpp set up as5600 and mpu6500

--- src/Arduino/main.cpp
+++ src/Arduino/main.cpp
@@ -9,15 +9,16 @@
 AS5600 as5600;
 MPU6500_WE mpu6500 = MPU6500_WE(MPU6500_ADDR);
 Servo servo_9;
+PidController controller(2.5, 0.75, 0.005); // Initialize the PID controller
 
 int servoPos = 0;
-PidController controller(2.5, 0.75, 0.005); // Initialize the PID controller
 String incomingByte;
 double target;
+xyzFloat gValue;
 double beta;
-double kos;
-double fx;
+double theta;
 double beta0;
+double theta0;
 
 void setup() {
     Serial.begin(115200); // Start the serial communication
@@ -26,6 +27,7 @@ void setup() {
 
     Wire.begin();
 
+    // Set up the AS5600
     as5600.begin(4);  //  set direction pin.
     as5600.setDirection(AS5600_CLOCK_WISE);  //  default, just be explicit.
     int b = as5600.isConnected();
@@ -39,6 +41,7 @@ void setup() {
 //        Serial.println("MPU6500 is connected");
     }
 
+    // Set up the MPU6500
     mpu6500.enableGyrDLPF();
     mpu6500.setGyrDLPF(MPU6500_DLPF_6);
     mpu6500.setSampleRateDivider(5);
@@ -48,48 +51,41 @@ void setup() {
     mpu6500.setAccDLPF(MPU6500_DLPF_6);
 
     delay(1000);
-//    beta0 = as5600.rawAngle() * AS5600_RAW_TO_DEGREES;
-    xyzFloat gValue = mpu6500.getGValues();
-    double beta0 = degrees(atan(gValue.y / (gValue.x * gValue.x + gValue.z * gValue.z)));;
-    beta = 0;
 
+    gValue = mpu6500.getGValues();
+    theta0 = degrees(atan(-gValue.x / (gValue.y * gValue.y + gValue.z * gValue.z)));
+    beta0 = as5600.rawAngle() * AS5600_RAW_TO_DEGREES;
+    beta = 0;
+    theta = 0;
 }
 
 void loop() {
     if (Serial.available() > 0) {
         // measure state
-        double old_beta = beta;
-//        double old_beta = as5600.rawAngle() * AS5600_RAW_TO_DEGREES - beta0;
+        gValue = mpu6500.getGValues();
+        double old_theta = degrees(atan(-gValue.x / (gValue.y * gValue.y + gValue.z * gValue.z))) - theta0;
+        beta = as5600.rawAngle() * AS5600_RAW_TO_DEGREES - beta0;
 
         // read target
         incomingByte = Serial.readStringUntil('\n');
         target = incomingByte.toDouble();
 
-        servoPos = min(75, max(0, target)); // direct control for testing
-
         // compute and execute control
-//        double controlForce = controller.computeControl(beta, target, 0.02);
+        servoPos = min(75, max(0, target)); // direct control for testing
+//        double controlForce = controller.computeControl(theta, target, 0.02);
 //        servoPos = degrees((controlForce / 0.25 + 37 * radians(beta)) / 15);
 //        servoPos = min(75, max(0, controlForce));
         servo_9.write(servoPos);
-//
-//        fx = (30 - kos) * 0.2;
-//        beta = degrees(
-//                radians(beta) + 0.5 * (-2 * fx * 52 + .25 * 37 * (15 * radians(servoPos) - 37 * radians(beta))) * 0.02 *
-//                                0.02); // dummy state
-//        beta = min(28, max(-9, beta));
-//
-        // measure state
-//        kos = sqrt(43 * 43 - pow(31 + 52 * radians(beta), 2)); //dummy state
-//        beta = as5600.rawAngle() * AS5600_RAW_TO_DEGREES - beta0;
+
         delay(20);
 
-        xyzFloat gValue = mpu6500.getGValues();
-//        double pitch = degrees(atan(gValue.x / (gValue.y * gValue.y + gValue.z * gValue.z)));
+        // measure state
+        gValue = mpu6500.getGValues();
+        theta = degrees(atan(-gValue.x / (gValue.y * gValue.y + gValue.z * gValue.z))) - theta0;
 //        double roll = degrees(atan(gValue.y / (gValue.x * gValue.x + gValue.z * gValue.z)));
-        double beta = degrees(atan(gValue.y / (gValue.x * gValue.x + gValue.z * gValue.z))); - beta0;
 
-        String message = String(old_beta) + " " + String(servoPos) + " " + String(beta);
+        // send to serial connection
+        String message = String(old_theta) + " " + String(servoPos) + " " + String(theta);
         Serial.println(message);
     }
 }
--- src/Arduino/pidControl.cpp
+++ src/Arduino/pidControl.cpp
@@ -7,8 +7,6 @@ PidController::~PidController() {}
 
 double PidController::computeControl(double state, double target, double dt) {
     double controlForce;
-//    int controlAngle;
-//    float max_force = 0.5 * (10 * kp_ + 10 * kd_ + 10 * ki_);
 
     double error = target - state;
     integral_error_ += error * dt;
@@ -16,9 +14,6 @@ double PidController::computeControl(double state, double target, double dt) {
     previous_error_ = error;
 
     controlForce = kp_ * error + ki_ * integral_error_ + kd_ * derivative_error;
-//    controlForce = max(0, min(controlForce, max_force));
-//    controlAngle = map(controlForce, 0, max_force, 0, 75);
-//    controlAngle = min(75, max(0, controlForce));
 
     return controlForce;
 }
