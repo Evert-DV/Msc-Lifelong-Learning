a8dae4b749322b0e87eedf6ebc4a9b10b9097655
Evert-DV <evertdevroey@gmail.com>
2024-06-19 16:54:20 +0200

    integrate target adapter

--- src/Arduino/serial_test.py
+++ src/Arduino/serial_test.py
@@ -1,21 +1,23 @@
+import os
+
+os.environ["KERAS_BACKEND"] = "torch"
 import threading
 import serial
-import keyboard
+# import keyboard
 import time
-import numpy as np
-from torch import ops
-from src.toy_problem.toy_tools import TargetAdapter
+from src.toy_problem.toy_tools import *
 
 target = -5
 arduino = None
 new_state = None
+new_omega = None
 freq = 1 / 50
-buffer = []
+recorded_data = []
 
 
-def save_buffer(name):
-    print("Saving buffer")
-    np.save(f"./Dynamics data/75-75 perforation/{name}.npy", buffer)
+def save_recorded_data(name):
+    print("Saving recorded_data")
+    np.save(f"./Dynamics data/75-75 perforation/{name}.npy", recorded_data)
 
 
 def send_value():
@@ -23,19 +25,20 @@ def send_value():
     global target
     while True:
         arduino.write(f'{target}\n'.encode())
-        time.sleep(freq)  # Send value at 50 Hz
+        time.sleep(freq)
 
 
 def listen_echo():
     global freq
-    global buffer
+    global recorded_data
     global new_state
+    global new_omega
 
     time.sleep(.5)
 
     while True:
         if arduino.in_waiting > 0:
-            # Read all data in the buffer
+            # Read all data in the recorded_data
             data = arduino.readlines(arduino.in_waiting)
 
             # Split the data into lines and take the last line
@@ -43,50 +46,58 @@ def listen_echo():
                 continue
 
             latest_value = data[-1].decode('utf-8').strip().split()
+            # latest value: old beta, old omega, control action, new beta, new omega
             old_state = float(latest_value[0])
-            control_action = float(latest_value[1])
-            new_state = float(latest_value[2])
+            old_omega = float(latest_value[1])
+            control_action = float(latest_value[2])
+            new_state = float(latest_value[-2])
+            new_omega = float(latest_value[-1])
 
             if time.time() % (1 / 4) < 0.03:
                 print(
-                    f"State: {old_state:.2f}\tControl action: {control_action:.0f}\tNew state: {new_state}\tTarget: {target:.0f}")
-            buffer.append([old_state, control_action, new_state, target])
+                    f"State: {old_state:.2f}, {old_omega:.1f}\tControl action: {control_action:.0f}"
+                    f"\tNew state: {new_state}, {new_omega:.2f}\tTarget: {target:.1f}")
+            recorded_data.append([old_state, old_omega, control_action, new_state, new_omega, target])
 
-        # if keyboard.is_pressed('s'):  # If 's' is pressed, save the buffer
-        #     save_buffer("user_save")
+        # if keyboard.is_pressed('s'):  # If 's' is pressed, save the recorded_data
+        #     save_recorded_data("user_save")
 
         time.sleep(freq)
-        # time.sleep(freq)  # Listen at 50 Hz
 
 
 def change_value():
     global target
     global new_state
+    global new_omega
     global freq
-    global buffer
+    global recorded_data
 
     start_time = time.time()
     old_t = time.time()
     count = 0
-    new_state = 30
+    new_state = 0
+    new_omega = 0
+    true_target = -5
 
-    adapter = TargetAdapter(state_size=1)
-    adapter.load_weights(f"./Models/adapter_weights.weights.h5")
+    adapter = TargetAdapter(state_size=2)
+    adapter.load_weights(f"./Models/adapter_weights_25.weights.h5")
 
     while True:
-        if time.time() - old_t > 2:
-            # true_target = np.random.randint(-22, 0)
-            true_target = -3 if true_target == -21 else -21
+        if time.time() - old_t > 7.5:
+            true_target = np.random.randint(-20, -5)
+            print(f"===== TRUE TARGET: {true_target} =====")
+            # true_target = -6 if true_target == -17 else -17
+            # target = true_target
             old_t = time.time()
 
-        if (time.time() - start_time) % 300 < 0.025:
-            save_buffer(f"fatigue_auto_save_{count}")
-            buffer = []
+        if (time.time() - start_time) % 300 < 0.025 and (time.time() - start_time) > 5:
+            save_recorded_data(f"b_auto_save_{count}")
+            # recorded_data = []
             count += 1
 
-        adapter_input = ops.array([*new_state, *true_target])[None]
-        delta_target = adapter.predict(adapter_input, verbose=0)[0]
-        target = target + delta_target
+        adapter_input = ops.array([new_state, new_omega, true_target, 0.])[None]
+        delta_target = adapter.predict(adapter_input, verbose=0)[0][0]
+        target = delta_target
 
         time.sleep(freq)
 
@@ -111,5 +122,12 @@ def main():
     target_thread.join()
 
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    print("Using backend " + keras.backend.backend())
+    os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
+    if torch.cuda.is_available():
+        print("Using CUDA")
+        with torch.cuda.device(0):
+            main()
+    else:
+        main()
