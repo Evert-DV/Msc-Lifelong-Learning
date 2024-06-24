7de347f626a6f26357ec294b9346b7823d3c316f
Evert-DV <evertdevroey@gmail.com>
2024-06-24 17:00:12 +0200

    viz true target

--- src/Arduino/visualize_data.py
+++ src/Arduino/visualize_data.py
@@ -2,19 +2,18 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-data = np.load("Dynamics data/75-75 perforation/user_save.npy")
+data = np.load("Dynamics data/75-75 perforation/auto_save_2.npy")
 signal = data[..., [0, 3]].ravel()
 control_action = data[..., [1, 1]].ravel()
-targets = data[..., [-1, -1]].ravel()
+targets = data[..., [-2, -2]].ravel()
+true_targets = data[..., [-1, -1]].ravel()
 
-fig, ax = plt.subplots(2, 1, figsize=(16, 8), sharex=True)
+fig, ax = plt.subplots(1, 1, figsize=(8, 4), sharex=True)
 
-ax[0].plot(signal, color='tab:blue', alpha=0.33)
-ax[0].plot(targets, '--', color='tab:gray', label="Target position")
-ax[0].legend(fontsize=8, loc='upper left')
-
-ax[1].plot(control_action, '--', color='tab:gray', label="Target position")
-ax[1].legend(fontsize=8, loc='upper left')
+ax.plot(signal, color='tab:blue', alpha=0.5)
+ax.plot(true_targets, '--', color='tab:gray', alpha=0.33, label="True target")
+ax.plot(targets, '--', color='tab:gray', label="Adapted target")
+ax.legend(fontsize=8, loc='upper left')
 
 fig.tight_layout()
 plt.show()
