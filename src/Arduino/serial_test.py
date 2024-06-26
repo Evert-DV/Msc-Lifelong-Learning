35c051230702b60de4633a1a1a3c0a3149d19117
Evert-DV <evertdevroey@gmail.com>
2024-06-26 14:57:46 +0200

    full implementation with adapter and knowledge base

--- src/Arduino/serial_test.py
+++ src/Arduino/serial_test.py
@@ -5,8 +5,16 @@ import threading
 import serial
 import keyboard
 import time
-from torch.utils.data import DataLoader
-from src.toy_problem.toy_tools import *
+import pickle
+import keras
+import torch
+import numpy as np
+from src.toy_problem.kb_tools import VariationalAutoEncoder, MultivariateNormal, sample, search_kb, js_divergence
+from src.toy_problem.toy_tools import TargetAdapter, RMSERegularizer, EpochLogger, prep_data
+from keras import ops, optimizers, losses
+from torch.utils.data import TensorDataset, DataLoader, random_split
+
+running = True
 
 arduino = None
 buffer_lock = None
@@ -23,9 +31,18 @@ recorded_data = []
 buffer = []
 
 # Load vanilla model
-prediction_window = 10
+prediction_window = 15
 adapter = keras.models.load_model(f'./Models/adapter_{prediction_window}.keras')
+
+# Load deployed model weights
 adapter.load_weights(f'./Models/deployed_adapter_{prediction_window}.weights.h5')
+
+# Load VAE model
+autoencoder = VariationalAutoEncoder(5, 2)  # keras.models.load_model("./Models/vae_skip_a.keras")
+autoencoder.load_weights("./Models/vae_skip_s.weights.h5")
+
+
+# autoencoder.compile(optimizer=optimizers.Adam(learning_rate=1.e-4), loss=losses.MeanSquaredError())
 
 
 def save_recorded_data(name):
@@ -40,11 +57,11 @@ def user_save():
     time.sleep(.5)
 
 
-
 def send_value():
-    while True:
+    while running:
         arduino.write(f'{target}\n'.encode())
         time.sleep(freq)
+    print("\nClosing `send_value` thread")
 
 
 def listen_echo():
@@ -56,20 +73,20 @@ def listen_echo():
     time.sleep(1)
     print_time = time.time()
 
-    while True:
+    while running:
         if arduino.in_waiting > 0:
             # Read all data in the recorded_data
             data = arduino.readlines(arduino.in_waiting)
-
             # Split the data into lines and take the last line
             if not data:
                 continue
 
             latest_value = data[-1].decode('utf-8').strip().split()
             # latest value: old beta, old omega, control action, new beta, new omega
-            old_state = float(latest_value[0])
-            old_omega = float(latest_value[1])
-            control_action = float(latest_value[2])
+            counter = int(latest_value[0])
+            old_state = float(latest_value[1])
+            old_omega = float(latest_value[2])
+            control_action = float(latest_value[3])
             new_state = float(latest_value[-3])
             new_omega = float(latest_value[-2])
             read_target = float(latest_value[-1])
@@ -80,14 +97,15 @@ def listen_echo():
                     f"\tNew state: {new_state}, {new_omega:.1f}\tTarget: {read_target:.1f}", end="")
                 print_time = time.time()
 
-            recorded_data.append([old_state, old_omega, control_action, new_state, new_omega, read_target, true_target])
+            recorded_data.append(
+                [counter, old_state, old_omega, control_action, new_state, new_omega, read_target, true_target])
 
             with buffer_lock:
                 buffer.append([old_state, old_omega, control_action, new_state, new_omega, read_target, true_target])
                 if len(buffer) > 3000:  # about a minute of data
                     buffer.pop(0)
 
-        # time.sleep(freq/4)
+    print("\nClosing `listen_echo` thread")
 
 
 def change_value():
@@ -95,16 +113,43 @@ def change_value():
     global true_target
     global recorded_data
 
+    # kb setup
+    use_kb = True
+
+    if use_kb:
+        try:
+            with open('kb.pkl', 'rb') as f:
+                kb = pickle.load(f)
+                reference = None
+                print(f"\n{len(kb[0])} entries loaded from the KB")
+        except FileNotFoundError:
+            reference_data = np.load(f"./Dynamics data/75-75 perforation/deployed_adapter_{prediction_window}.npy")
+            x_reference = ops.array(reference_data)[..., [1, 2, 3, 4, 5]].reshape(-1, 5)
+            z_mean, z_log_var = autoencoder.dynamics(x_reference)
+            cov = sample(z_mean, z_log_var)[1]
+            reference = MultivariateNormal(z_mean, cov)
+            kb = [[reference], [adapter.get_weights()]]
+
+        running_distribution = None
+        updated_reference = None
+        backup_updated_reference = None
+        current_kb_idx = None
+
+        thres = np.log(2) / 2
+
+        first_selection = True
+
     # set epoch
     start_time = time.time()
     target_t = start_time
     save_t = start_time
+    kb_t = start_time
+    update_t = start_time
 
     count = 0
-    n = 0
 
     print(f"\n{10 * '='} TRUE TARGET: {true_target} {10 * '='}")
-    while True:
+    while running:
         t0 = time.time()
 
         adapter_input = ops.array([new_state, new_omega, true_target, 0.])[None]
@@ -113,14 +158,8 @@ def change_value():
 
         if time.time() - target_t > 5:
             true_target = np.random.randint(-23, -2)
-            # if n % 2 == 0:
-            #     true_target = np.random.randint(-23, -15)
-            # else:
-            #     true_target = np.random.randint(-10, -2)
-            # n += 1
 
             print(f"\n{10 * '='} TRUE TARGET: {true_target} {10 * '='}")
-            # true_target = -6 if true_target == -17 else -17  # oscillate
             # target = true_target
             target_t = time.time()
 
@@ -128,12 +167,128 @@ def change_value():
             save_recorded_data(f"auto_save_{count}")
             with update_lock:
                 adapter.save_weights(f'./Models/deployed_adapter_{prediction_window}.weights.h5')
-            # recorded_data = []
             count += 1
             save_t = time.time()
 
+        # KB step
+        if not use_kb:
+            dt = time.time() - t0
+            time.sleep(max(0, freq - dt))
+            continue
+
+        if time.time() - kb_t > 5:
+            kb_t = time.time()
+
+            # Get embeddings
+            with buffer_lock:
+                z_mean, z_log_var = autoencoder.dynamics(ops.array(buffer)[..., [0, 1, 2, 3, 4]])
+            embeddings, cov = sample(z_mean, z_log_var, samples_per_centroid=1)
+
+            # Pre-60 seconds
+            if time.time() - start_time < 60:
+                running_distribution = MultivariateNormal(z_mean, cov)
+                current_kb_idx, js_divs = search_kb(running_distribution, kb[0])
+                continue
+
+            if first_selection:
+                # Select KB entry
+                reference = kb[0][current_kb_idx]
+                updated_reference = reference.copy()
+                backup_updated_reference = reference.copy()
+                with update_lock:
+                    adapter.set_weights(kb[1][current_kb_idx])
+                print(f"\nSelected KB entry {current_kb_idx} as reference")
+                first_selection = False
+
+            # Post-60 seconds
+            # Update running distribution
+            running_distribution.update(z_mean, cov, weight=5 / (60 - 5))
+
+            # KL losses
+            kl_updated_dist = js_divergence(updated_reference, running_distribution)
+            kl_reference_updated = js_divergence(reference, updated_reference)
+            # kl_loss.append([kl_updated_dist.item(), kl_reference_updated.item()])
+
+            # Check for shift
+            if kl_updated_dist > thres or kl_reference_updated > .75 * thres:
+                # retain last or leave it as it was?
+                # kb[0][kb_idx] = backup_updated_reference.copy()
+                # kb[1][kb_idx] = adapter.get_weights()
+                best_idx, js_divs = search_kb(running_distribution, kb[0])
+                if best_idx != current_kb_idx and js_divs[best_idx] < thres:
+                    print(f"\n{34 * '='}\n"
+                          f"||        SHIFT DETECTED        ||\n"
+                          f"||  Restore KB entry reference  ||\n"
+                          f"||   Check KB for better match  ||\n"
+                          f"|| Use KB entry {best_idx} as reference{(3 - len(str(best_idx))) * ' '}||\n"
+                          f"{34 * '='}")
+                    reference = kb[0][best_idx]
+                    updated_reference = reference.copy()
+                    backup_updated_reference = reference.copy()
+                    current_kb_idx = best_idx
+                    with update_lock:
+                        adapter.set_weights(kb[1][best_idx])
+                else:
+                    print(f"\n{34 * '='}\n"
+                          f"||        SHIFT DETECTED        ||\n"
+                          f"||  Restore KB entry reference  ||\n"
+                          f"||  Check KB for better match   ||\n"
+                          f"||        No match found        ||\n"
+                          f"||     Initiate new KB entry    ||\n"
+                          f"{34 * '='}")
+                    reference = running_distribution.copy()
+                    updated_reference = reference.copy()
+                    kb[0].append(reference)
+                    with update_lock:
+                        kb[1].append(adapter.get_weights())
+                    current_kb_idx = len(kb[0]) - 1
+
+        # KB update step
+        if time.time() - update_t > 60 and not first_selection:
+            torch.cuda.empty_cache()
+            best_idx, js_divs = search_kb(running_distribution, kb[0])
+            if best_idx != current_kb_idx and js_divs[best_idx] < thres:
+                print(f"\n{34 * '='}\n"
+                      f"||         UPDATE STEP          ||\n"
+                      f"||  Check KB for better match   ||\n"
+                      f"|| Use KB entry {best_idx} as reference ||\n"
+                      f"{34 * '='}")
+                kb[0][current_kb_idx] = backup_updated_reference  # restore the distribution
+                with update_lock:
+                    kb[1][current_kb_idx] = adapter.get_weights()  # update the weights
+                reference = kb[0][best_idx]
+                updated_reference = reference.copy()
+                backup_updated_reference = reference.copy()
+                current_kb_idx = best_idx
+                with update_lock:
+                    adapter.set_weights(kb[1][best_idx])
+                continue
+
+            # Update reference
+            print(f"\n{34 * '='}\n"
+                  f"||         UPDATE STEP          ||\n"
+                  f"||  Check KB for better match   ||\n"
+                  f"||        No match found        ||\n"
+                  f"||      Updated reference       ||\n"
+                  f"{34 * '='}")
+            backup_updated_reference = updated_reference.copy()
+            updated_reference.update(z_mean, cov, weight=0.1)
+
+            update_t = time.time()
+
         dt = time.time() - t0
         time.sleep(max(0, freq - dt))
+
+    # Save the last updated reference
+    kb[0][current_kb_idx] = backup_updated_reference
+    with update_lock:
+        kb[1][current_kb_idx] = adapter.get_weights()
+
+    with open('kb.pkl', 'wb') as f:
+        pickle.dump(kb, f)
+
+    print(f"\n{len(kb[0])} entries saved in the KB\nClosing `change_value` thread")
+    save_recorded_data(f"deployed_adapter_{prediction_window}")
 
 
 def update_adapter():
@@ -145,14 +300,15 @@ def update_adapter():
     train_t = start_time
 
     temp_adapter = TargetAdapter(state_size=2)
+    temp_adapter.regularizer.add(RMSERegularizer(weight=.1))
+    temp_adapter.compile(optimizer=keras.optimizers.Adam(learning_rate=5e-5),
+                         loss=keras.losses.MeanSquaredError())
 
-    while True:
+    while running:
         if time.time() - train_t > 15:
             print(f"\n{26 * '='}\n===   Updating model   ===\n{26 * '='}")
 
             temp_adapter.set_weights(adapter.get_weights())
-            temp_adapter.compile(optimizer=keras.optimizers.Adam(learning_rate=1e-4),
-                                 loss=keras.losses.MeanSquaredError())
 
             with buffer_lock:
                 update_buffer = ops.array(buffer)[..., :-1]
@@ -185,6 +341,7 @@ def update_adapter():
             train_t = time.time()
 
         time.sleep(freq)
+    print("\nClosing `update_adapter` thread")
 
 
 def main():
@@ -194,6 +351,7 @@ def main():
     arduino = serial.Serial('COM5', 115200)
 
     keyboard.add_hotkey('ctrl+s', user_save)
+    keyboard.add_hotkey('ctrl+q', lambda: globals().update(running=False))
 
     buffer_lock = threading.Lock()
     update_lock = threading.Lock()
