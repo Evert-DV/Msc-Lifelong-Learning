490d45552a79f04dde634a9536091321391f3400
Evert-DV <evertdevroey@gmail.com>
2024-07-10 09:27:11 +0200

    restructure code

--- src/Arduino/analyze_data.py
+++ src/Arduino/analyze_data.py
@@ -3,7 +3,8 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 adapter_window = 25
-file = f"deployed_adapter_{adapter_window}"
+# file = f"deployed_adapter_{adapter_window}"
+file = "auto_save_0"
 data = np.load(f"Dynamics data/75-75 perforation/{file}.npy")
 count = data[..., [0]].ravel()
 beta = data[..., [1]].ravel()
@@ -18,32 +19,6 @@ try:
     js_div_vals, js_div_counts, selection_counts, trespass_counts, update_counts = plot_counters
 except FileNotFoundError:
     js_div_vals, js_div_counts, selection_counts, trespass_counts, update_counts = [[], []], [], [], [], []
-
-fig, ax = plt.subplots(4, 1, figsize=(12, 8), sharex=True)
-
-ax[0].plot(count, beta, marker='.', color='tab:blue', alpha=0.5, markersize=3, label="Angle")
-ax[0].plot(count, targets, '--', color='tab:gray', alpha=0.33, label="Adapted target")
-ax[0].plot(count, true_targets, '--', color='tab:gray', label="True target")
-ax[0].legend(fontsize=8, loc='upper left')
-
-ax[1].plot(count, omega, marker='.', color='tab:orange', alpha=0.5, markersize=3, label="Angular velocity")
-ax[1].legend(fontsize=8, loc='upper left')
-
-ax[2].plot(count, control_action, marker='.', color='tab:green', alpha=0.5, markersize=3, label="Control action")
-ax[2].legend(fontsize=8, loc='upper left')
-
-ax[3].plot(js_div_counts, js_div_vals, marker='.', markersize=3., lw=1., alpha=0.7,
-           label=['Running distribution vs. updated reference', 'Updated reference vs. Backed-up reference'])
-ax[3].vlines(update_counts, 0., np.log(2), color='lightgrey', linestyle='-', label='Updates')
-ax[3].vlines(selection_counts, 0., np.log(2), color='tab:green', linestyle='--', label='KB selections')
-ax[3].vlines(trespass_counts, 0., np.log(2), color='tab:red', linestyle=':', label='Trespasses')
-ax[3].axhline(np.log(2) / 2, color='tab:blue', alpha=.5, linestyle='--', lw=1.)
-ax[3].axhline(np.log(2) / 4, color='tab:orange', alpha=.5, linestyle='--', lw=1.)
-ax[3].legend(fontsize=8, loc='upper left')
-
-fig.tight_layout()
-plt.savefig(f'tmp/{file}.png')
-plt.show()
 
 dt = np.diff(count / 50)  # approximated dt
 target_windows = np.argwhere(np.diff(true_targets) != 0).ravel() + 1
@@ -109,3 +84,32 @@ for metric, value in zip(metrics, values):
         print("| {:<20} | {:<20.4f} |".format(metric, value))
     else:
         print("| {:<20} | {:<20} |".format(metric, "Array"))
+
+
+fig, ax = plt.subplots(4, 1, figsize=(12, 8), sharex=True)
+
+ax[0].plot(count, beta, marker='.', color='tab:blue', alpha=0.5, markersize=3, label="Angle")
+ax[0].plot(count, targets, '--', color='tab:gray', alpha=0.33, label="Adapted target")
+ax[0].plot(count, true_targets, '--', color='tab:gray', label="True target")
+ax[0].legend(fontsize=8, loc='upper left')
+
+ax[1].plot(count, omega, marker='.', color='tab:orange', alpha=0.5, markersize=3, label="Angular velocity")
+ax[1].legend(fontsize=8, loc='upper left')
+
+ax[2].plot(count, control_action, marker='.', color='tab:green', alpha=0.5, markersize=3, label="Control action")
+ax[2].legend(fontsize=8, loc='upper left')
+
+ax[3].plot(js_div_counts, js_div_vals, marker='.', markersize=3., lw=1., alpha=0.7,
+           label=['Running distribution vs. updated reference', 'Updated reference vs. Backed-up reference'])
+ax[3].vlines(update_counts, 0., np.log(2), color='lightgrey', linestyle='-', label='Updates')
+ax[3].vlines(selection_counts, 0., np.log(2), color='tab:green', linestyle='--', label='KB selections')
+ax[3].vlines(trespass_counts, 0., np.log(2), color='tab:red', linestyle=':', label='Trespasses')
+ax[3].axhline(np.log(2) / 2, color='tab:blue', alpha=.5, linestyle='--', lw=1.)
+ax[3].axhline(np.log(2) / 4, color='tab:orange', alpha=.5, linestyle='--', lw=1.)
+ax[3].legend(fontsize=8, loc='upper left')
+
+fig.tight_layout()
+plt.savefig(f'tmp/{file}.png')
+plt.show()
+
+
