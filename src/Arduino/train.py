6019b91a97d534f4cca4d28a130a97a4334df8b2
Evert-DV <evertdevroey@gmail.com>
2024-07-24 12:07:02 +0200

    LR reducing upon plateauing

--- src/Arduino/train.py
+++ src/Arduino/train.py
@@ -9,19 +9,19 @@ from src.toy_problem.toy_tools import TargetAdapter, prep_data
 
 
 def main():
-    pretrain = False
-    seed = np.random.randint(0, 1000)
-    # seed = 63
+    pretrain = True
+    # seed = np.random.randint(0, 1000)
+    seed = 63
     print(f"Seed: {seed}")
     np.random.seed(seed)
     torch.manual_seed(seed)
     if pretrain:
         torch.manual_seed(16)
 
-    prediction_window = 5
+    prediction_window = 3  # [1, 3, 5, 10, 15, 25]
 
-    model_location = './Models/'
-    model_name = f'adapter_5.keras'
+    model_location = './Models/crawling gait/'
+    model_name = f'adapter_{prediction_window}.keras'
 
     adapter = TargetAdapter(state_size=2)
 
@@ -31,19 +31,25 @@ def main():
         adapter.summary()
         print("Model loaded")
 
-    optimizer = keras.optimizers.Adam(learning_rate=1e-4)
-    loss_fn = keras.losses.MeanSquaredError()
+    optimizer = keras.optimizers.Adam(learning_rate=1e-3)
+    loss_fn = keras.losses.MeanAbsoluteError()
     adapter.compile(optimizer=optimizer, loss=loss_fn)
 
-    pretrain_data = np.load(f"./Dynamics data/75-75 perforation/pretrain_data.npy")
+    pretrain_data = np.load(f"./Dynamics data/crawling gait/pretrain.npy")
     pretrain_data = pretrain_data[..., 1:]  # Remove the counter
     train_set, val_set = prep_data(pretrain_data, prediction_window, state_size=2, val_split=0.2)
     train_dataloader = DataLoader(train_set, batch_size=256, shuffle=True)
     val_dataloader = DataLoader(val_set, batch_size=256, shuffle=False)
 
-    callbacks = [keras.callbacks.EarlyStopping(monitor='val_loss',
+    callbacks = [keras.callbacks.ReduceLROnPlateau(monitor='val_loss',
+                                                   factor=0.1,
+                                                   patience=7,
+                                                   min_lr=5e-5,
+                                                   min_delta=1e-3,
+                                                   verbose=0),
+                 keras.callbacks.EarlyStopping(monitor='val_loss',
                                                mode='min',
-                                               min_delta=1e-4,
+                                               min_delta=1e-3,
                                                patience=10,
                                                restore_best_weights=True,
                                                verbose=1),
